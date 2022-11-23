15f637da11825a263a0930d3cff23bdfdd94b14d
Navani-cpu <doolocompany@gmail.com>
2022-11-23 19:02:16 +0530

    feat(fingerprint): added fingerprint screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,107 @@
-import { Redirect, Route } from 'react-router-dom';
+import {Redirect, Route} from "react-router-dom"
 import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+	IonApp,
+	IonIcon,
+	IonLabel,
+	IonRouterOutlet,
+	IonContent,
+	IonTabBar,
+	IonTabButton,
+	IonTabs,
+	useIonAlert,
+	IonButton,
+	setupIonicReact,
+} from "@ionic/react"
+import {IonReactRouter} from "@ionic/react-router"
+import {ellipse, square, triangle} from "ionicons/icons"
+import Tab1 from "./pages/Tab1"
+import Tab2 from "./pages/Tab2"
+import Tab3 from "./pages/Tab3"
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css"
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css"
+import "@ionic/react/css/structure.css"
+import "@ionic/react/css/typography.css"
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css"
+import "@ionic/react/css/float-elements.css"
+import "@ionic/react/css/text-alignment.css"
+import "@ionic/react/css/text-transformation.css"
+import "@ionic/react/css/flex-utils.css"
+import "@ionic/react/css/display.css"
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css"
 
-setupIonicReact();
+//fingerprint
+import {NativeBiometric} from "capacitor-native-biometric"
+import React, {useState, useEffect} from "react"
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+setupIonicReact()
 
-export default App;
+const App: React.FC = () => {
+	const [data, setData] = useState("")
+	const [presentAlert] = useIonAlert()
+	const [handlerMessage, setHandlerMessage] = useState("")
+	const [roleMessage, setRoleMessage] = useState("")
+	const fingerPrint = async () => {
+		const result: any = await NativeBiometric.isAvailable()
+		const verified = await NativeBiometric.verifyIdentity({
+			reason: "For easy log in",
+			title: "Log in",
+			subtitle: "Maybe add subtitle here?",
+			description: "Maybe a description too?",
+		})
+			.then(() => true)
+			.catch(() => false)
+
+		console.log("result :: ", result)
+		alert(result.biometryType)
+		alert(verified)
+	}
+
+	useEffect(() => {
+		fingerPrint()
+	}, [])
+
+	return (
+		<IonApp>
+			<>
+				<IonButton
+					onClick={() =>
+						presentAlert({
+							header: "Alert!",
+							buttons: [
+								{
+									text: "Cancel",
+									role: "cancel",
+									handler: () => {
+										setHandlerMessage("Alert canceled")
+									},
+								},
+								{
+									text: "OK",
+									role: "confirm",
+									handler: () => {
+										setHandlerMessage(data)
+									},
+								},
+							],
+							onDidDismiss: (e: CustomEvent) =>
+								setRoleMessage(`Dismissed with role: ${e.detail.role}`),
+						})
+					}>
+					Click Me
+				</IonButton>
+				<p>{handlerMessage}</p>
+				<p>{roleMessage}</p>
+			</>
+		</IonApp>
+	)
+}
+
+export default App
